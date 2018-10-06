838a3b13e8f652aea774dd19e9e17d7f1a090b25
Yuvaraj Muthuraman <yuva@Yuvarajs-MacBook-Air.local>
2018-10-06 23:46:23 +0200

    added get category by id implementation

--- src/main/java/com/ebuyer/backoffice/api/controller/CategoryController.java
+++ src/main/java/com/ebuyer/backoffice/api/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class CategoryController {
 	@Autowired
 	private CategoryRepository repository;
 	
-	@RequestMapping(value="categories", method=RequestMethod.GET)
+	@RequestMapping(value="/categories", method=RequestMethod.GET)
 	public List<Category> getCategories() {
 		List<Category> categories = new ArrayList<Category>();
 		
@@ -25,5 +26,10 @@ public class CategoryController {
 		});
 		
 		return categories;
+	}
+	
+	@RequestMapping(value="/category/{id}", method=RequestMethod.GET)
+	public Category getCategory(@PathVariable Long id) {
+		return repository.findById(id).get();
 	}
 }
